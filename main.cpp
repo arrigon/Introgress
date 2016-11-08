ae6f3e86c5905e27f31082a04006a4c9b3689321
arrigon <nils.arrigo@gmail.com>
2016-11-08 19:37:57 +0100

    update

--- main.cpp
+++ main.cpp
@@ -11,37 +11,75 @@ using namespace std;
 using namespace arma;
 
 
-int main()
+int main(int argc, char *argv[])
 {
-    // initiate variables
-
-    // 16 genes: mu = 0.1, omega = 0.8 / 0.5, full matrix, 100iter conv, eps = 1e-3
     arma::arma_rng::set_seed_random(); // TO UNCOMMENT BEFORE PROD change the random seed
-    int n_genes = 8;                    // number of genes in network
-    double networkFillness = .5;      // proportion of non-null interactions in the network
-    int connectGradient = 1;
 
-    int niter_conv = 100;               // number of iterations in convergence test
-    int n_phens = 25;
+    //DEBUG SWITCH ///////
+    int debugMode = 1;  // WARNING: debugMode = 0 for PROD !!!
+    //////////////////////
 
-    int n_gams = 500;
-    int n_indiv = 100;
-    int n_generations1 = 50;
-    int n_generations2 = 200;
-    int n_generations3 = 30;
 
-    double epsilon = 1e-5;
-    double self_rate = 0;
-    double backcross_rate = 0;
-    double mut_rate = 0.05;
-    double omega = 0.8;
-    double mindist = 0.05;
+    //// Initiate variables
+    int n_genes, niter_conv, n_phens, n_gams, n_indiv, n_generations1, n_generations2, n_generations3, connectGradient;                 // number of genes in network
+    double networkFillness, epsilon, self_rate, backcross_rate, mut_rate, omega, mindist;      // proportion of non-null interactions in the network
 
-    rowvec phen_opt = randu<rowvec>(n_genes);
+
+    // initiate variables DEBUG MODE ONLY
+    if(debugMode == 1)
+    {
+        n_genes = 49;                 // number of genes in network
+        networkFillness = .5;      // proportion of non-null interactions in the network
+        connectGradient = 1;          // set a connectivity gradient in the gene network
+
+        niter_conv = 100;             // nb of iterations in convergence test
+        n_phens = 25;                 // nb of phenotypes we keep in convergence tests
+        epsilon = 1e-5;            // convergence criterion
+
+        n_gams = 500;                 // nb of gametes available per individual
+        n_indiv = 100;                // nb of individuals per population
+
+        n_generations1 = 50;          // nb of generations in training phase
+        n_generations2 = 200;         // nb of generations in divergence phase
+        n_generations3 = 30;          // nb of generations in hybridization phase
+
+        self_rate = 0;             // selfing rate
+        backcross_rate = 0;        // backcrossing rate
+        mut_rate = 0.05;           // mutation rate (during phases 1 and 2, set to 0 via hard coding in hybridization phase)
+        omega = 0.8;               // selection coefficient
+        mindist = 0.05;            // (used instead of n_generations1, = minimum phenotypic distance to reach before moving to divergence phase)
+    }
+
+
+    // Initiate variables PRODUCTION MODE
+    if(debugMode == 0)
+    {
+        n_genes = atoi(argv[1]);                 // number of genes in network
+        networkFillness = atof(argv[2]);      // proportion of non-null interactions in the network
+        connectGradient = 1;                     // set a connectivity gradient in the gene network
+
+        niter_conv = atoi(argv[3]);             // nb of iterations in convergence test
+        n_phens = atoi(argv[4]);                // nb of phenotypes we keep in convergence tests
+        epsilon = atof(argv[10]);            // convergence criterion
+
+        n_gams = atoi(argv[5]);                 // nb of gametes available per individual
+        n_indiv = atoi(argv[6]);                // nb of individuals per population
+
+        n_generations1 = atoi(argv[7]);          // nb of generations in training phase
+        n_generations2 = 200;         // nb of generations in divergence phase
+        n_generations3 = 30;          // nb of generations in hybridization phase
+
+        self_rate = atof(argv[11]);             // selfing rate
+        backcross_rate = 0;        // backcrossing rate
+        mut_rate = atof(argv[13]);  // 13 ou 14 ??           // mutation rate (during phases 1 and 2, set to 0 via hard coding in hybridization phase)
+        omega = atof(argv[14]);               // selection coefficient
+        mindist = 0.05;            // (used instead of n_generations1, = minimum phenotypic distance to reach before moving to divergence phase)
+    }
 
 
     //// Initiating pop
     // Finding initial convergent network
+    cout << "main: n_genes =\n" << n_genes << endl;
     mat w(n_genes, n_genes);
     Network net;
     net.loadParams(w,
@@ -51,6 +89,7 @@ int main()
                     mut_rate);
     w = net.getConvergentNetwork(networkFillness, connectGradient);
     cout << "main: convergent network =\n" << w << endl;
+
 
 
 //    // Load it into founder individual
@@ -80,6 +119,7 @@ int main()
 //
 //
 //    // Canalize on first phenotype
+//    rowvec phen_opt(n_genes);
 //    phen_opt.load("phen_opt_alien.txt");         // Load phen_opt form outfile
 //
 //    double distToOpt = 1;
